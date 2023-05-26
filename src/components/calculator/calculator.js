412aeb7ae4f9a4fa9e95b68283b7cd65bc863c57
Sina Haghshenas <103804601+Sina-Hgs@users.noreply.github.com>
2023-05-26 22:28:20 +0330

    trying the API

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -1,7 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
 import "./calculator.css";
 
 const Calculator = () => {
-  return <div>Calculator</div>;
+  return (
+    <div>
+      <FontAwesomeIcon icon={faArrowRightArrowLeft} size="2xl" />
+    </div>
+  );
 };
 
 export default Calculator;
