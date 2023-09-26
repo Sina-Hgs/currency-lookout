458c9e1439389a23063e40acf4c7e58185b2e126
Sina Haghshenas <103804601+Sina-Hgs@users.noreply.github.com>
2023-09-27 01:11:52 +0330

    added a notice

--- src/App.js
+++ src/App.js
@@ -15,6 +15,27 @@ const App = () => {
       <div className="App dark">
         <span className="fade-in-span">
           <Header />
+          <div className="notice">
+            <button
+              onClick={() =>
+                (document.querySelector(".notice").style.display = "none")
+              }
+            >
+              &#10006;
+            </button>
+            <h3>Attention!</h3>
+            If the app doesn't show any data it may be because the free API that
+            I used to make this app might become pay to use in the near future.
+            <br />
+            I'll work on a new version of this app that uses another API if the
+            current one stops working for free. The updated version will be
+            available on this page when it's done.
+            <br />
+            If the app is broken right now, feel free to peek at the code in the
+            mean time (GitHub link available down on the footer).
+            <br />
+            Thankyou!
+          </div>
           <DataChart />
           <Timing />
           <Calculator />
